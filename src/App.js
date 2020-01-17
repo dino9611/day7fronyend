fcc0870b9eeee2c7e4e68355063583ab2abb5729
Dino <aldinorahman36@gmail.com>
2020-01-17 14:36:54 +0700

    dasd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, {useEffect,Fragment,useState} from 'react';
+import React, {useEffect,Fragment,useState, useRef} from 'react';
 import './App.css';
 import Axios from 'axios'
-import {APIURL} from './helper/apiurl'
-import { Table } from 'reactstrap';
+import {APIURL, APIURLimage} from './helper/apiurl'
+import { Table,CustomInput } from 'reactstrap';
 import Modal from './components/modal'
 
 // let didupdatejalan=0
@@ -13,7 +13,17 @@ function App() {
   const [roles,setroles]=useState([])
   const [modal, setModal] = useState(false);
   const [modaldelete, setModaldelete] = useState(false);
-
+  const [modaladd, setmodaladd] = useState(false);
+  const [addimagefile,setimageadd]=useState({
+    addImageFileName:'Select Image....',
+    addImageFile:undefined,
+  })
+  const [dataadd,setdataadd]=useState({
+    username:useRef(),
+    email:useRef(),
+    usia:useRef(),
+    roleid:useRef()
+  })
   const toggle = () =>{
     setModal(!modal)
   };
@@ -21,7 +31,12 @@ function App() {
     setdatausersedit(datausers[index])
     setModal(true)
   };
+  // const openmodaladd = (index) =>{
+  //   setdatausersedit(datausers[index])
+  //   setModal(true)
+  // };
   const toggledelete=()=>setModaldelete(!modaldelete)
+  const toggleadd=()=>setmodaladd(!modaladd)
 
 
   useEffect(()=>{
@@ -44,6 +59,7 @@ function App() {
       <tr key={index}>
         <th scope="row">{index+1}</th>
         <td>{val.username}</td>
+        <td><img src={`${APIURLimage+val.image}`} height='150px'/></td>
         <td>{val.email}</td>
         <td>{val.phone}</td>
         <td>{val.usia}</td>
@@ -61,10 +77,11 @@ function App() {
       )
     })
   }
-  console.log(datausersedit)
+
+
   
   const Updatedata=()=>{
-    console.log(datausersedit)
+    // console.log(datausersedit)
     var data={
       username:datausersedit.username,
       email:datausersedit.email,
@@ -80,18 +97,71 @@ function App() {
       console.log(err)
     })
   }
-
+  const onAddImageFileChange=(event)=>{
+    // console.log(document.getElementById('addImagePost').files[0])
+    console.log(event.target.files[0])
+    var file=event.target.files[0]
+    if(file){
+        setimageadd({...addimagefile,addImageFileName:file.name,addImageFile:event.target.files[0]})
+    }else{
+        setimageadd({...addimagefile,addImageFileName:'Select Image...',addImageFile:undefined})
+    }
+}
+  const adddata=()=>{
+    var formdata=new FormData()
+    const {username,roleid,usia,email}=dataadd
+    const data={
+      username:username.current.value,
+      roleid:roleid.current.value,
+      usia:usia.current.value,
+      password:'rahasia',
+      email:email.current.value,
+    }
+    var Headers={
+      headers:
+      {
+          'Content-Type':'multipart/form-data',
+      }
+  }
+    formdata.append('image',addimagefile.addImageFile)
+    formdata.append('data',JSON.stringify(data))
+    Axios.post(`${APIURL}postusers`,formdata,Headers)
+    .then((res)=>{
+      setdatausers(res.data.datauser)
+      setroles(res.data.datarole) 
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
   if(datausers.length===0){
     return <div>loading</div>
   }
   return (
     <Fragment>
-      <button>add data</button>
+      <button onClick={toggleadd}>add data</button>
       <Modal title='edit data' toggle={toggle} modal={modal} actionfunc={Updatedata}>
         <input type='text' className='form-control' value={datausersedit.username} onChange={e=>setdatausersedit({...datausersedit,username:e.target.value})}/>
         <input type='text' className='form-control' value={datausersedit.email} onChange={e=>setdatausersedit({...datausersedit,email:e.target.value})}/>
         <input type='number' className='form-control' value={datausersedit.usia} onChange={e=>setdatausersedit({...datausersedit,usia:e.target.value})}/>
         <select className='form-control' value={datausersedit.roleid} onChange={e=>setdatausersedit({...datausersedit,roleid:e.target.value})}>
+          <option hidden>piliih category</option>
+          {
+            roles.map((val,index)=>{
+              return(
+              <option key={index} value={val.id}>
+                {val.nama}
+              </option>
+              )
+            })
+          }
+        </select>
+      </Modal>
+      <Modal title='add data' toggle={toggleadd} modal={modaladd} actionfunc={adddata} >
+        <input type='text' placeholder='username' className='form-control' ref={dataadd.username} />
+        <input type='text' placeholder='email' className='form-control' ref={dataadd.email}/>
+        <input type='number' placeholder='usia' className='form-control' ref={dataadd.usia} />
+        <CustomInput type='file' label={addimagefile.addImageFileName} id='addImagePost' className='form-control' onChange={onAddImageFileChange} />
+        <select className='form-control' ref={dataadd.roleid}>
           <option hidden>piliih category</option>
           {
             roles.map((val,index)=>{
@@ -112,6 +182,7 @@ function App() {
           <tr>
             <th>#</th>
             <th>username</th>
+            <th>image</th>
             <th>email</th>
             <th>phone</th>
             <th>usia</th>
