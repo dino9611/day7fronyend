0a921b86bcaccf201c9ad5fe42f87491aad73d1c
Dino <aldinorahman36@gmail.com>
2020-01-21 10:49:14 +0700

    dasd

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ import './supports/fontawesome-free/css/fontawesome.min.css'
 import './supports/fontawesome-free/css/all.css'
 import './supports/css/style.css'
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import {BrowserRouter} from 'react-router-dom'
+ReactDOM.render(
+<BrowserRouter>
+    <App />
+</BrowserRouter>   
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
