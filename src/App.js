448c03894ea1b5dcaad933f0132a3d0b3cf2570a
Dino <aldinorahman36@gmail.com>
2020-01-23 10:39:04 +0700

    dasd

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import ManageUsers from './pages/manageusers'
 import Register from './pages/register'
 import Header from './components/header'
 import verifikasi from './pages/verified'
+import resendVerif from './pages/waitingverified'
+import login from './pages/login'
 // let didupdatejalan=0
 function App() {
   
@@ -16,6 +18,8 @@ function App() {
       <Switch>
         <Route path='/' exact component={ManageUsers}/>
         <Route path='/register' exact component={Register}/>
+        <Route path='/resendverif' exact component={resendVerif}/>
+        <Route path='/login' exact component={login}/>
         <Route path='/verified' exact component={verifikasi}/>
       </Switch>
     </Fragment>
